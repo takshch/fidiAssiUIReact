72e233c898ad50ce0b628df8e6efae637aca2999
takshch <tkcphp@gmail.com>
2020-06-24 16:55:08 +0530

    checks id exits or not

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 var database, collection;
 
 var autoIncrement = require("mongodb-autoincrement");
-const { response } = require("express");
+const { response, request } = require("express");
 
 app.listen(5000, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
@@ -34,56 +34,75 @@ app.get("/items", (request, response) => {
     });
 });
 
-app.get("/add", (request, response) => {
-    collection.find({},{projection:{_id:0}}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
 
 app.post("/add",(request,response)=>{
-    getNextSequence(database, COLLECTION_NAME, function(err, result){
-        if(!err){
-            database.collection(COLLECTION_NAME).insertOne({id: result, ...request.body},(error,result)=>{
-                if(error){
-                    return response.status(500).send(error);
-                } 
-                if(result.result.ok === 1){
-                    response.send({status: "success", error: null});
-                }else{
-                    response.send(result.result);
-                }
-            });
-        }
-        else{
-            response.send({error: "Something is not right!"});
-        }
-    });
+    if(("name" in request.body) && ("price" in request.body) && !isNaN(request.body.price)){
+        getNextSequence(database, COLLECTION_NAME, function(err, result){
+            if(!err){
+                database.collection(COLLECTION_NAME).insertOne({id: result, ...request.body},(error,result)=>{
+                    if(error){
+                        return response.status(500).send(error);
+                    } 
+                    if(result.result.ok === 1){
+                        response.send({status: "success", error: null});
+                    }else{
+                        response.send(result.result);
+                    }
+                });
+            }
+            else{
+                response.send({error: "Something is not right!"});
+            }
+        });
+    }else if(!("name" in request.body) || !("price" in request.body) || request.body.name === "" || request.body.price === ""){
+        response.send({error: "fields are missing"});
+    }else{
+        response.send({error: "price must be number"});
+    } 
 });
 
 app.post("/update",(req,response)=>{
-    collection.updateOne({id: req.body.id},{$set:{price: req.body.price}},(error,result)=>{
-        if(error){
-            response.send({error: "Something is not right!"});
+    if(("id" in req.body) && ("price" in req.body) && req.body.id !== "" && req.body.price !== "" && !isNaN(req.body.id)  && !isNaN(req.body.price)){
+        if(collection.find({id: req.body.id}).count() > 0){
+            collection.updateOne({id: req.body.id},{$set:{price: req.body.price}},(error,result)=>{
+                if(error){
+                    response.send({error: "Something is not right!"});
+                }
+                else if(result){
+                    response.send({status: "success",error: null});
+                }
+            });
+        }else{
+            response.send({error: "id doesn't exits"});
         }
-        else if(result){
-            response.send({status: "success",error: null});
-        }
-    });
+    }else if(!("id" in req.body) || !("price" in req.body) || req.body.id === "" || req.body.price === ""){
+        response.send({error: "fields are missing"});
+    }
+    else{
+        response.send({error: "id and price must be number"});
+    }
 });
 
 app.post("/delete",(req, response)=>{
-    collection.deleteOne({id: req.body.id},(error,result)=>{
-        if(error){
-            response.send({error: "Something is not right!"});
-        }else if(result.deletedCount === 1){
-            response.send({status: "deleted",error: null});
-        }
-    });
-});
 
+    if(("id" in req.body) && !isNaN(req.body.id) && req.body.id !== ""){
+        if(collection.find({id: req.body.id}).count() > 0){
+            collection.deleteOne({id: req.body.id},(error,result)=>{
+                if(error){
+                    response.send({error: "Something is not right!"});
+                }else if(result.deletedCount === 1){
+                    response.send({status: "deleted",error: null});
+                }
+            });
+        }else{
+            response.send({error: "id doesn't exits"})
+        }
+    }else if(!("id" in req.body) || req.body.id === ""){
+        response.send({error: "fields are missing"});
+    }else{
+        response.send({error: "id must be number"});
+    }
+});
 
 
 //AutoIncrement Function for MongoDB.....
